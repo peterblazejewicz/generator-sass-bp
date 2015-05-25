1c24c2d02e164c876aa8d10b72643a78266c0c2c
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2015-05-25 22:09:17 +0200

    :white_check_mark: Update test coverage
    Add test to verify that no content is created when user chooses to not run generator

--- test/test-app.js
+++ test/test-app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var assert = require('yeoman-generator').assert;
 var helpers = require('yeoman-generator').test;
 
-describe('sass-bp:app', function() {
+describe('sass-bp:app generate all content', function() {
   before(function(done) {
     helpers.run(path.join(__dirname, '../app'))
       .withOptions({
@@ -78,4 +78,80 @@ describe('sass-bp:app', function() {
     ]);
   });
 
+});
+
+describe('sass-bp:app generate no content', function() {
+  before(function(done) {
+    helpers.run(path.join(__dirname, '../app'))
+      .withOptions({
+        skipInstall: true
+      })
+      .withPrompts({
+        generateAll: false
+      })
+      .on('end', done);
+  });
+
+  it('does not create base stylesheets', function() {
+    assert.noFile([
+      'stylesheets/base/_base.scss',
+      'stylesheets/base/_fonts.scss',
+      'stylesheets/base/_helpers.scss',
+      'stylesheets/base/_typography.scss',
+      'stylesheets/base/README.md'
+    ]);
+  });
+
+  it('does not create components stylesheets', function() {
+    assert.noFile([
+      'stylesheets/components/_buttons.scss',
+      'stylesheets/components/README.md'
+    ]);
+  });
+
+  it('does not create layout stylesheets', function() {
+    assert.noFile([
+      'stylesheets/layout/_footer.scss',
+      'stylesheets/layout/_header.scss',
+      'stylesheets/layout/README.md'
+    ]);
+  });
+
+  it('does not create pages stylesheets', function() {
+    assert.noFile([
+      'stylesheets/pages/_home.scss',
+      'stylesheets/pages/README.md'
+    ]);
+  });
+
+  it('does not create themes stylesheets', function() {
+    assert.noFile([
+      'stylesheets/themes/_default.scss',
+      'stylesheets/themes/README.md'
+    ]);
+  });
+
+  it('does not create utils stylesheets', function() {
+    assert.noFile([
+      'stylesheets/utils/_functions.scss',
+      'stylesheets/utils/_mixins.scss',
+      'stylesheets/utils/_variables.scss',
+      'stylesheets/utils/README.md'
+    ]);
+  });
+
+  it('does not create vendor stylesheets', function() {
+    assert.noFile([
+      'stylesheets/vendor/_normalize.scss',
+      'stylesheets/vendor/README.md'
+    ]);
+  });
+
+  it('does not create main stylesheet', function() {
+    assert.noFile([
+      'stylesheets/main.scss',
+      'stylesheets/README.md'
+    ]);
+  });
+  
 });
